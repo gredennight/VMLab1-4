08c3c8022009758172dae02d255803684d2c9cd3
grede <grede@GREDEN>
2022-02-14 20:22:25 +0200

    Добавьте файлы проекта.

---
+++ ЛабЛаб1/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ЛабЛаб1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBarY = new System.Windows.Forms.TrackBar();
+            this.trackBarX = new System.Windows.Forms.TrackBar();
+            this.trackBarZoom = new System.Windows.Forms.TrackBar();
+            this.ZoomText = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxX0 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Compute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(807, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(662, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(810, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(467, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(468, 318);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "очистить вывод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(736, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0,001";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(733, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Точность";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(477, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "масштаб";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // trackBarY
+            // 
+            this.trackBarY.Location = new System.Drawing.Point(405, 4);
+            this.trackBarY.Maximum = 5;
+            this.trackBarY.Minimum = -5;
+            this.trackBarY.Name = "trackBarY";
+            this.trackBarY.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarY.Size = new System.Drawing.Size(56, 383);
+            this.trackBarY.TabIndex = 12;
+            this.trackBarY.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarY.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBarX
+            // 
+            this.trackBarX.Location = new System.Drawing.Point(12, 382);
+            this.trackBarX.Maximum = 5;
+            this.trackBarX.Minimum = -5;
+            this.trackBarX.Name = "trackBarX";
+            this.trackBarX.Size = new System.Drawing.Size(383, 56);
+            this.trackBarX.TabIndex = 13;
+            this.trackBarX.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarX.Scroll += new System.EventHandler(this.trackBarX_Scroll);
+            // 
+            // trackBarZoom
+            // 
+            this.trackBarZoom.Location = new System.Drawing.Point(467, 28);
+            this.trackBarZoom.Minimum = 1;
+            this.trackBarZoom.Name = "trackBarZoom";
+            this.trackBarZoom.Size = new System.Drawing.Size(189, 56);
+            this.trackBarZoom.TabIndex = 14;
+            this.trackBarZoom.Value = 1;
+            this.trackBarZoom.Scroll += new System.EventHandler(this.trackBarZoom_Scroll);
+            // 
+            // ZoomText
+            // 
+            this.ZoomText.AutoSize = true;
+            this.ZoomText.Location = new System.Drawing.Point(553, 68);
+            this.ZoomText.Name = "ZoomText";
+            this.ZoomText.Size = new System.Drawing.Size(0, 16);
+            this.ZoomText.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "половинный",
+            "Ньютона",
+            "секущих",
+            "простой итерации"});
+            this.comboBox1.Location = new System.Drawing.Point(777, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 24);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBoxX0
+            // 
+            this.textBoxX0.Location = new System.Drawing.Point(881, 29);
+            this.textBoxX0.Name = "textBoxX0";
+            this.textBoxX0.Size = new System.Drawing.Size(65, 22);
+            this.textBoxX0.TabIndex = 18;
+            this.textBoxX0.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(878, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "x0";
+            // 
+            // Compute
+            // 
+            this.Compute.Location = new System.Drawing.Point(662, 56);
+            this.Compute.Name = "Compute";
+            this.Compute.Size = new System.Drawing.Size(103, 33);
+            this.Compute.TabIndex = 19;
+            this.Compute.Text = "вычислить";
+            this.Compute.UseVisualStyleBackColor = true;
+            this.Compute.Click += new System.EventHandler(this.Compute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 450);
+            this.Controls.Add(this.Compute);
+            this.Controls.Add(this.textBoxX0);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ZoomText);
+            this.Controls.Add(this.trackBarZoom);
+            this.Controls.Add(this.trackBarX);
+            this.Controls.Add(this.trackBarY);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBarY;
+        private System.Windows.Forms.TrackBar trackBarX;
+        private System.Windows.Forms.TrackBar trackBarZoom;
+        private System.Windows.Forms.Label ZoomText;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxX0;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Compute;
+    }
+}
+
---
+++ ЛабЛаб1/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ЛабЛаб1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// получить у для заданого х
+        /// </summary>
+        public double Function(double x)
+        {
+            return Math.Pow(x,4)+2*Math.Pow(x,3)-x-1;
+        }
+        /// <summary>
+        /// спрятать все доп элементы
+        /// </summary>
+        void HideAll()
+        {
+            textBox1.Visible = false;
+            textBox2.Visible=false;
+            textBoxX0.Visible=false;
+            label1.Visible=false;
+            label2.Visible=false;
+            label4.Visible=false;
+        }
+        /// <summary>
+        /// получить значение касательной
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public double FunctionShtrih(double x)
+        {
+            return 4*Math.Pow(x,3)+6*Math.Pow(x,2)-1;
+        }
+
+        /// <summary>
+        /// отрисовка графика
+        /// </summary>
+        public void PrintGraph()
+        {
+            Graphics screen;
+            screen=CreateGraphics();
+            screen.Clear(Color.White);
+            int max = 300;
+            int x1=0, y1=0, x2=0, y2=0;
+            double zoom =Convert.ToDouble( trackBarZoom.Value);
+            ///на вход идёт значение х в пикселях, на выход - у тоже в пикселях
+            int GetY(int x)
+            {
+                double mashtab = max/(zoom*2);
+                double realX = (x/mashtab)-zoom;
+                double realY = Function(realX);
+                double y = (realY+zoom)*mashtab;
+
+                return Convert.ToInt32(max-y);
+            }
+            ///отрисовка координат
+            screen.DrawLine(Pens.Black, max/2, 0, max/2, max);
+            screen.DrawLine(Pens.Black, 0, max/2, max, max/2);
+
+
+            ///отрисовка графика
+            for (int i = 0; i < max; i++)
+            {
+                y1 =GetY(i);
+                x1=i;
+                screen.DrawLine(Pens.Red, x1, y1, x2, y2);
+                y2=y1;
+                x2=x1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        //метод половинного деления
+        private void Polovinki()
+        {
+            PrintGraph();
+            double a0 = Convert.ToDouble(textBox1.Text);
+            double b0 = Convert.ToDouble(textBox2.Text);
+            double E = Convert.ToDouble(textBox3.Text);
+            int count = 0;
+
+            richTextBox1.Text+="итерация а b f1 f2 f3";
+
+            while (Math.Abs(b0-a0)>2*E)
+            {
+                double c = (a0+ b0)/2;
+                double f1 = Function(a0);
+                double f2 = Function(c);
+                double f3 = Function(b0);
+
+                //вывод в консоль
+                richTextBox1.Text+="\n№"+count+" a:"+Math.Round(a0, 4)+" b:"+Math.Round(b0, 4)+" F(a):"+Math.Round(f1, 4)+" F(c):"+Math.Round(f2, 4)+" F(b):"+Math.Round(f3, 4);
+
+                //выбор интервала для следующей итерации
+                if (f1<0 && f2>0)
+                {
+                    double a1 = a0;
+                    double b1 = c;
+                    a0=a1;
+                    b0=b1;
+
+                }
+                else
+                {
+                    double a1 = c;
+                    double b1 = b0;
+                    a0=a1;
+                    b0=b1;
+
+                }
+                count++;
+
+            }
+            richTextBox1.Text+="\nВ результате треша и экшена получаем, что х="+(a0+b0)/2;
+        }
+
+        //метод ньютона
+        private void Newton()
+        {
+            double x0=Convert.ToDouble( textBoxX0.Text);
+            double x1 = x0;
+            double E = Convert.ToDouble(textBox3.Text);
+            do
+            {
+                x0 = x1;
+                double f = Function(x0);
+                double F = FunctionShtrih(x0);
+                x1=x0-(f/F);
+                //вставить сюда вывод значений
+                richTextBox1.Text+="\nx0:"+Math.Round(x0, 3)+" x1:"+Math.Round(x1, 3)+" f:"+Math.Round(f, 3)+" F:"+Math.Round(F, 3);
+
+            }
+            while (Math.Abs(x1-x0)>E);
+            richTextBox1.Text+="\nВ результате треша и экшена получаем, что х="+x1;
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            PrintGraph();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBarZoom_Scroll(object sender, EventArgs e)
+        {
+            PrintGraph();
+            ZoomText.Text=Convert.ToString( trackBarZoom.Value);
+        }
+
+        private void trackBarX_Scroll(object sender, EventArgs e)
+        {
+            PrintGraph();
+        }
+
+        /// <summary>
+        /// скрытие ненужных элементов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0://половинного деления
+                    HideAll();
+                    textBox1.Visible = true;
+                    textBox2.Visible=true;
+                    textBox3.Visible=true;
+                    label1.Visible=true;
+                    label2.Visible=true;
+                    label3.Visible=true;
+                    break;
+                case 1://ньютона
+                    HideAll();
+                    textBoxX0.Visible=true;
+                    label4.Visible=true;
+                    break;
+
+                case 2://секущих
+                    break;
+
+                case 3://итераций
+                    break;
+            }
+        }
+        /// <summary>
+        /// вычисление значения на основе выбранного режима
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Compute_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0://половинного деления
+                    Polovinki();
+                    break;
+                case 1://ньютона
+                    Newton();
+                    break;
+
+                case 2://секущих
+                    break;
+
+                case 3://итераций
+                    break;
+            }
+        }
+    }
+}
